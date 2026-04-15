e39129f53b88c015afe785edc045ed4d17cf8c25
SENSAI <farooqrizwan476@gmail.com>
2026-04-15 12:14:53 -0700

    add predefined files of button tabs and such that type of things

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
-import Image from "next/image";
+import  {Button}  from "@/components/ui/button";
 
 export default function Home() {
- return  <div>PIVOT</div> ;
+ return  <div>PIVOT
+ <br/>
+ <Button>Hello</Button>
+ </div> ;
     
 }
